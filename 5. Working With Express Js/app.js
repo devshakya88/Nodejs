68a7bbc23c548de340d0742ab8e6a833251edd0a
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-16 09:53:58 +0530

    Express

---
+++ 5. Working With Express Js/app.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In the another  Middleware');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>hello from expres</h1>');
+});
+
+app.listen(3000);
