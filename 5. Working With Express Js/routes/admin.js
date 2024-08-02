e9c5c00f99a5297f44d31bbd2d654c316f22ae09
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-08-02 09:21:06 +0530

    Pug Template Done

--- 5. Working With Express Js/routes/admin.js
+++ 5. Working With Express Js/routes/admin.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const rootDir = require('../utils/path');
 
 const products = [];
 
-//Admin add product - GET
+// Admin add product - GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product' });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product', // Pass the current path
+  });
 });
 
-//Admin add Product - POST
+// Admin add product - POST
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect('/');
--- 5. Working With Express Js/routes/shop.js
+++ 5. Working With Express Js/routes/shop.js
@@ -1,13 +1,15 @@
-const path = require('path');
 const express = require('express');
-const rootDir = require('../utils/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, docTitle: 'Shop' });
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/', // Pass the current path
+  });
 });
 
 module.exports = router;
