63e724cb49dffae268757081280dbb3a367a3fb7
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-08-02 08:46:45 +0530

    Adding Pug template

--- 5. Working With Express Js/app.js
+++ 5. Working With Express Js/app.js
@@ -15,7 +15,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 app.listen(3000);
--- 5. Working With Express Js/routes/admin.js
+++ 5. Working With Express Js/routes/admin.js
@@ -7,7 +7,7 @@ const products = [];
 
 //Admin add product - GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 //Admin add Product - POST
